e25f224752c78ac1e9dc8d2960adaaf696272187
jccasti8 <jccasti8@cougarnet.uh.edu>
2024-06-28 18:45:06 -0500

    added a home.jsx and minor udpates

--- volunteer-app/src/App.jsx
+++ volunteer-app/src/App.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 import Header from './Header';
 import Login from './Login';
 import Registration from './Registration';
@@ -16,6 +17,7 @@ function App() {
       <Header />
       <div className="main-content">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/profile" element={<Profile />} />
@@ -23,7 +25,7 @@ function App() {
         <Route path="/event-management" element={<EventManage />} />
         <Route path="/notifications" element={<Notifications />} />
         
-        <Route path="/" element={<div><h2>The Volunteer</h2></div>} />
+       
 
 
       </Routes>
